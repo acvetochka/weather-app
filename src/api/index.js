89741155880b87c12bd8c2289faf6deea320e53f
Alona Kuznietsova <acvetochka@gmail.com>
2024-06-10 21:56:26 +0200

    change secrets

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,7 @@
 import axios from "axios"
 
-const API_KEY = "03592c99ed3af1a8ee625d106921133f"
-const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
 
 export function getWeatherData(city) {
-    return axios.get(`${BASE_URL}?q=${city}&appid=${API_KEY}`)
+    return axios.get(`${process.env.BASE_URL}?q=${city}&appid=${process.env.API_KEY}`)
 }
