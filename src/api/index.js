56007e9a8f9924807d7d3b11adf34dc7967be562
Alona Kuznietsova <acvetochka@gmail.com>
2024-06-11 21:35:52 +0200

    change api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 export function getWeatherData(city) {
-    return axios.get(`${process.env.BASE_URL}?q=${city}&appid=${process.env.API_KEY}`)
+    return axios.get(`${BASE_URL}?q=${city}&appid=${process.env.API_KEY}`)
 }
