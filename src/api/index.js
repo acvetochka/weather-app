14eb975c434dfe4531361669c72a2d59ebe0e903
Alona Kuznietsova <acvetochka@gmail.com>
2024-06-11 21:56:12 +0200

    add secret

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 
-const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+const VUE_APP_BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 export function getWeatherData(city) {
-    return axios.get(`${BASE_URL}?q=${city}&appid=${process.env.API_KEY}`)
+    return axios.get(`${VUE_APP_BASE_URL}?q=${city}&appid=${process.env.VUE_APP_API_KEY}`)
 }
